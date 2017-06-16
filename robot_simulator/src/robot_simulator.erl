0447310dd42a4cb66c9795ea197ad709a29ed058
ekmopqa <jianrong.zhang@ericsson.com>
2017-06-16 11:08:44 +0200

    Robot simulator phase 1, second version

--- robot_simulator/src/robot_simulator.erl
+++ robot_simulator/src/robot_simulator.erl
@@ -11,36 +11,34 @@ control(RobotState, [H | T]) ->
     NewRobotState = execute_command(RobotState, H),
     control(NewRobotState, T).
 
-execute_command(#robot_state{coordinate = Coordinate, direction = Direction}, Turn) when Turn == $l; Turn == $r ->
-    NewDirection = change_direction(Direction, Turn),
-    #robot_state{coordinate = Coordinate, direction = NewDirection};
-execute_command(#robot_state{coordinate = Coordinate, direction = Direction}, Move) when Move == $f; Move == $b ->
-    NewCoordinate = move(Coordinate, Direction, Move),
-    #robot_state{coordinate = NewCoordinate, direction = Direction};
-execute_command(RobotState, _) ->
-    RobotState.
+execute_command(Robot, Turn) when Turn == $l orelse Turn == $r ->
+    NewDirection = change_direction(Robot#robot_state.direction, Turn),
+    Robot#robot_state{direction = NewDirection};
+execute_command(Robot, Move) when Move == $f orelse Move == $b ->
+    NewCoordinate = move(Robot#robot_state.coordinate, Robot#robot_state.direction, Move),
+    Robot#robot_state{coordinate = NewCoordinate};
+execute_command(Robot, _) ->
+    Robot.
 
 change_direction(Direction, Turn) ->
-    %           current, left, right
-    DirecTab = [{north, west, east},
-		{east, north, south},
-	        {south, east, west},
-	        {west, south, north}],
-    {_, LeftDirection, RightDirection} = lists:keyfind(Direction, 1, DirecTab),
-    case Turn == $l of
-	true ->
-	    LeftDirection;
-	false ->
-	    RightDirection
-    end.
+    DirecTab = [{north, [{$l, west}, {$r, east}]},
+		{east, [{$l, north}, {$r, south}]},
+		{south, [{$l, east}, {$r, west}]},
+		{west, [{$l, south}, {$r, north}]}],
+    Directions = proplists:get_value(Direction, DirecTab),
+    proplists:get_value(Turn, Directions).
 
-move(#coordinate{x = X, y = Y}, Direct, Move) when Direct == east, Move == $f; Direct == west, Move == $b ->
-    #coordinate{x = X + 1, y = Y};
-move(#coordinate{x = X, y = Y}, Direct, Move) when Direct == east, Move == $b; Direct == west, Move == $f ->
-    #coordinate{x = X - 1, y = Y};
-move(#coordinate{x = X, y = Y}, Direct, Move) when Direct == north, Move == $f; Direct == south, Move == $b ->
-    #coordinate{x = X, y = Y + 1};
-move(#coordinate{x = X, y = Y}, Direct, Move) when Direct == north, Move == $b; Direct == south, Move == $f ->
-    #coordinate{x = X, y = Y - 1}.
+move(Coord, Direct, Move) when Direct == east, Move == $f; Direct == west, Move == $b ->
+    NewX = Coord#coordinate.x + 1,
+    Coord#coordinate{x = NewX};
+move(Coord, Direct, Move) when Direct == east, Move == $b; Direct == west, Move == $f ->
+    NewX = Coord#coordinate.x - 1,
+    Coord#coordinate{x = NewX};
+move(Coord, Direct, Move) when Direct == north, Move == $f; Direct == south, Move == $b ->
+    NewY = Coord#coordinate.y + 1,
+    Coord#coordinate{y = NewY};
+move(Coord, Direct, Move) when Direct == north, Move == $b; Direct == south, Move == $f ->
+    NewY = Coord#coordinate.y - 1,
+    Coord#coordinate{y = NewY}.
     
     
--- robot_simulator/test/robot_simulator_tests.erl
+++ robot_simulator/test/robot_simulator_tests.erl
@@ -3,28 +3,49 @@
 -include("robot_simulator.hrl").
 
 initialize_robot_test() ->
-    RobotInitState = robot_simulator:initialize({7,3}, north),
+    RobotInitState = robot_simulator:initialize({7, 3}, north),
     ?assertEqual(
        #robot_state{coordinate = #coordinate{x = 7, y = 3}, direction = north}, 
        RobotInitState).
 
-rotate_robot_test() ->
-    RobotInitState = robot_simulator:initialize({7,3}, north),
+rotate_robot_left_test() ->
+    RobotInitState = robot_simulator:initialize({-98, 35}, south),
     RobotState = robot_simulator:control(RobotInitState, "l"),
     ?assertEqual(
-       #robot_state{coordinate = #coordinate{x = 7, y = 3}, direction = west}, 
+       #robot_state{coordinate = #coordinate{x = -98, y = 35}, direction = east}, 
        RobotState).
 
-move_robot_test() ->
-    RobotInitState = robot_simulator:initialize({7,3}, north),
+rotate_robot_right_test() ->
+    RobotInitState = robot_simulator:initialize({25, -35}, west),
+    RobotState = robot_simulator:control(RobotInitState, "r"),
+    ?assertEqual(
+       #robot_state{coordinate = #coordinate{x = 25, y = -35}, direction = north}, 
+       RobotState).
+
+move_robot_backward_test() ->
+    RobotInitState = robot_simulator:initialize({-136, -57}, east),
     RobotState = robot_simulator:control(RobotInitState, "b"),
     ?assertEqual(
-       #robot_state{coordinate = #coordinate{x = 7, y = 2}, direction = north}, 
+       #robot_state{coordinate = #coordinate{x = -137, y = -57}, direction = east}, 
        RobotState).
 
-control_robot_test() ->
+move_robot_forward_test() ->
+    RobotInitState = robot_simulator:initialize({45, 93}, west),
+    RobotState = robot_simulator:control(RobotInitState, "f"),
+    ?assertEqual(
+       #robot_state{coordinate = #coordinate{x = 44, y = 93}, direction = west}, 
+       RobotState).
+
+rotate_and_move_robot_test() ->
     RobotInitState = robot_simulator:initialize({7,3}, north),
     RobotState = robot_simulator:control(RobotInitState, "rfflflb"),
     ?assertEqual(
        #robot_state{coordinate = #coordinate{x = 10, y = 4}, direction = west}, 
+       RobotState).
+
+invalid_command_test() ->
+    RobotInitState = robot_simulator:initialize({39, -21}, south),
+    RobotState = robot_simulator:control(RobotInitState, "fxl"),
+    ?assertEqual(
+       #robot_state{coordinate = #coordinate{x = 39, y = -22}, direction = east}, 
        RobotState).
