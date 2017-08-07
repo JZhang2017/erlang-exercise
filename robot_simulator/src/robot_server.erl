605e57cb71668827827df5c10a58b95c5fb45765
ekmopqa <jianrong.zhang@ericsson.com>
2017-08-07 15:40:09 +0200

    Robot simulator phase 3, second version
    Improvement

--- robot_simulator/src/robot_server.erl
+++ robot_simulator/src/robot_server.erl
@@ -1,19 +1,49 @@
 -module(robot_server).
--export([robot_server/0, recv/1]).
+-export([start/1, stop/1, supervisor/1, listener/1, transaction_handler/1]).
 
-robot_server() ->
-    {ok, ListenSocket} = gen_tcp:listen(8888, [binary, {active, false}]),
+start(Port) ->
+    spawn(?MODULE, supervisor, [Port]).
+
+stop(SupervisorPid) ->
+    SupervisorPid ! stop.
+
+supervisor(Port) ->
+    spawn_link(?MODULE, listener, [Port]),
+    receive
+	stop ->
+	    exit(ordered_exit)
+    end.
+
+listener(Port) ->
+    {ok, ListenSocket} = gen_tcp:listen(Port, [binary, {active, true}]),
     accept(ListenSocket).
 
 accept(ListenSocket) ->
-    {ok, AcceptSocket} = gen_tcp:accept(ListenSocket),
-    spawn(?MODULE, recv, [AcceptSocket]),
-    accept(ListenSocket).
+    Result = gen_tcp:accept(ListenSocket),
+    case Result of
+	{ok, AcceptSocket} ->
+	    Handler = spawn_link(?MODULE, transaction_handler, [AcceptSocket]),
+	    gen_tcp:controlling_process(AcceptSocket, Handler),
+	    accept(ListenSocket);
+	{error, closed} ->
+	    ok;
+	 _ ->
+	   accept(ListenSocket)
+    end.
 
-recv(Socket) ->
-    {ok, Request} = gen_tcp:recv(Socket, 0),
-    Msg = process_request(binary_to_term(Request)),
-    ok = gen_tcp:send(Socket, term_to_binary(Msg)).
+transaction_handler(Socket) ->
+    receive
+	{tcp_closed, _} ->
+	    gen_tcp:close(Socket);
+	{tcp_error, _, _} ->
+	    gen_tcp:close(Socket);
+	{tcp, _, Request} ->
+	   Msg = process_request(binary_to_term(Request)),
+	   ok = gen_tcp:send(Socket, term_to_binary(Msg)),
+	    transaction_handler(Socket);
+	_ ->
+	   transaction_handler(Socket)
+    end.
 
 process_request({init, {Coordinate, Direction}}) ->
     robot_simulator:initialize(Coordinate, Direction);
@@ -23,9 +53,3 @@ process_request(_) ->
     % Obviously it can't handle all the abnormal case.
     % Just crash for other abnormal cases.
     unknown_command.
-
-
-	    
-	    
-
-    
--- robot_simulator/test/robot_simulator_socket_SUITE.erl
+++ robot_simulator/test/robot_simulator_socket_SUITE.erl
@@ -1,7 +1,8 @@
 -module(robot_simulator_socket_SUITE).
 -include_lib("common_test/include/ct.hrl").
+-include_lib("eunit/include/eunit.hrl").
 -include("robot_simulator.hrl").
--export([all/0, groups/0, init_per_suite/1]).
+-export([all/0, groups/0, init_per_suite/1, end_per_suite/1]).
 -export([initialize_robot/1, rotate_robot_left/1, rotate_robot_right/1,
 	 move_robot_forward/1, move_robot_backward/1, rotate_and_move_robot/1,
 	 handle_invalid_msg/1, rotate_and_move_robot_with_invalid_command/1]).
@@ -20,57 +21,64 @@ groups() ->
 			rotate_and_move_robot_with_invalid_command]}].
 
 init_per_suite(Config) ->
-    spawn(robot_server, robot_server, []),
-    Config.
+    ct:pal("Start server"),
+    ServerPid = robot_server:start(8888),
+    ConfigWithSeverPid = [{server_pid, ServerPid} | Config],
+    [{port, 8888} | ConfigWithSeverPid].
 
-initialize_robot(_) ->
-    Reply = contact_server({init, {{28, -7}, west}}),
-    #robot_state{coordinate = #coordinate{x = 28, y = -7}, direction = west} == Reply.
+end_per_suite(Config) ->
+     ct:pal("Stop server"),
+    ServerPid = ?config(server_pid, Config),
+    robot_server:stop(ServerPid).
 
-rotate_robot_left(_) ->
+initialize_robot(Config) ->
+    Reply = contact_server({init, {{28, -7}, west}}, ?config(port, Config)),
+    ?assertEqual(#robot_state{coordinate = #coordinate{x = 28, y = -7}, direction = west}, Reply).
+
+rotate_robot_left(Config) ->
     State = #robot_state{coordinate = #coordinate{x = -98, y = 35}, direction = south},
     Command = "l",
-    Reply = contact_server({control, State, Command}),
-    #robot_state{coordinate = #coordinate{x = -98, y = 35}, direction = east} == Reply.
+    Reply = contact_server({control, {State, Command}}, ?config(port, Config)),
+    ?assertEqual(#robot_state{coordinate = #coordinate{x = -98, y = 35}, direction = east}, Reply).
 
-rotate_robot_right(_) ->
+rotate_robot_right(Config) ->
     State = #robot_state{coordinate = #coordinate{x = 25, y = -35}, direction = west},
     Command = "r",
-    Reply = contact_server({control, State, Command}),
-    #robot_state{coordinate = #coordinate{x = 25, y = -35}, direction = north} == Reply.
+    Reply = contact_server({control, {State, Command}}, ?config(port, Config)),
+    ?assertEqual(#robot_state{coordinate = #coordinate{x = 25, y = -35}, direction = north}, Reply).
 
-move_robot_forward(_) ->
+move_robot_forward(Config) ->
     State = #robot_state{coordinate = #coordinate{x = 45, y = 93}, direction = west},
     Command = "f",
-    Reply = contact_server({control, State, Command}),
-    #robot_state{coordinate = #coordinate{x = 44, y = 93}, direction = west} == Reply.
+    Reply = contact_server({control, {State, Command}}, ?config(port, Config)),
+    ?assertEqual(#robot_state{coordinate = #coordinate{x = 44, y = 93}, direction = west}, Reply).
 
-move_robot_backward(_) ->
+move_robot_backward(Config) ->
     State = #robot_state{coordinate = #coordinate{x = -136, y = -57}, direction = east},
     Command = "b",
-    Reply = contact_server({control, State, Command}),
-    #robot_state{coordinate = #coordinate{x = -137, y = -57}, direction = east} == Reply.
+    Reply = contact_server({control, {State, Command}}, ?config(port, Config)),
+    ?assertEqual(#robot_state{coordinate = #coordinate{x = -137, y = -57}, direction = east}, Reply).
 
-rotate_and_move_robot(_) ->
+rotate_and_move_robot(Config) ->
     State = #robot_state{coordinate = #coordinate{x = 7, y = 3}, direction = north},
     Commands = "rfflflb",
-    Reply = contact_server({control, State, Commands}),
-    #robot_state{coordinate = #coordinate{x = 10, y = 4}, direction = west} == Reply.
+    Reply = contact_server({control, {State, Commands}}, ?config(port, Config)),
+    ?assertEqual(#robot_state{coordinate = #coordinate{x = 10, y = 4}, direction = west}, Reply).
 
-handle_invalid_msg(_) ->
-    Reply = contact_server("What's up"),
-    unknown_command == Reply.
+handle_invalid_msg(Config) ->
+    Reply = contact_server("What's up", ?config(port, Config)),
+    ?assertEqual(unknown_command, Reply).
 
-rotate_and_move_robot_with_invalid_command(_) ->
+rotate_and_move_robot_with_invalid_command(Config) ->
     State = #robot_state{coordinate = #coordinate{x = 39, y = -21}, direction = south},
     Commands = "fxl",
-    Reply = contact_server({control, State, Commands}),
-    #robot_state{coordinate = #coordinate{x = 39, y = -22}, direction = east} == Reply.   
+    Reply = contact_server({control, {State, Commands}}, ?config(port, Config)),
+    ?assertEqual(#robot_state{coordinate = #coordinate{x = 39, y = -22}, direction = east}, Reply).
 
-contact_server(Request) ->
+contact_server(Request, Port) ->
     ct:pal("Start to connect"),
     {ok, Socket} = gen_tcp:connect({127,0,0,1},
-				   8888, 
+				   Port, 
 				   [binary, {active,false}]),
     ok = gen_tcp:send(Socket, term_to_binary(Request)),
     {ok, Reply} = gen_tcp:recv(Socket, 0),
